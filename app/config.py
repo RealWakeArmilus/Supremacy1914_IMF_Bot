236f2f274232ec40af130c1129090db8bacfa081
Emil Artemev <158851444+RealWakeArmilus@users.noreply.github.com>
2025-02-22 16:59:16 +0300

    TICK-0051: Mega Upgrade (v. 0.2.0.1)
    Add from config.py:
    - CHANNEL_USERNAME

--- app/config.py
+++ app/config.py
@@ -7,8 +7,10 @@ import os
 load_dotenv()
 
 # Получаем токен из переменной окружения
-TOKEN_BOT = os.getenv("TELEGRAM_BOT_TOKEN")
+TOKEN_BOT = os.getenv("TEST_TELEGRAM_BOT_TOKEN")
 print(f"Бот использует токен: {TOKEN_BOT[:5]}... (скрыт)")
+
+CHANNEL_USERNAME = '@Supremacy1914_IMF_Channel'
 
 TON_WALLET_SPACE = 'UQDfYF3iMQDxc_JZBuuU_5gotKtQ-f0rjaIuuuZFYHKsGjYt'
 TON_WALLET_TONKEEPER = 'UQBjgJlI4tPRypKgiYQ3YWYXLrNSa4aW_lwlXCqDuo60XOA0'
@@ -16,3 +18,6 @@ TON_WALLET_TONKEEPER = 'UQBjgJlI4tPRypKgiYQ3YWYXLrNSa4aW_lwlXCqDuo60XOA0'
 
 bot = Bot(token=TOKEN_BOT)
 dp = Dispatcher()
+
+
+
